c33e7c53eaf0b35235039c85fcde151995d55eae
Beck Busch <beckbusch5@gmail.com>
2023-03-15 16:57:56 +1300

    Created NIOS Project, recompiled quartus, made changes to quartus programmer.
    Created NIOS Project,
    fixed quartus board programmer by changing settings, required recompile
    added code to NIOS, built and ran on board, success
    
    Correct Quartus programmer layout:
    SOCVHPS, no file, nothing ticked, first in block diagram
    5CSEMA5F31, sof file, program ticked, second in block diagram

---
+++ Lab_1_Quartus/software/Lab_1/obj/default/binary_led.d
@@ -0,0 +1,13 @@
+obj/default/binary_led.o: binary_led.c ../Lab_1_bsp/system.h \
+ ../Lab_1_bsp/linker.h ../Lab_1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Lab_1_bsp//HAL/inc/io.h ../Lab_1_bsp//HAL/inc/alt_types.h
+
+../Lab_1_bsp/system.h:
+
+../Lab_1_bsp/linker.h:
+
+../Lab_1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Lab_1_bsp//HAL/inc/io.h:
+
+../Lab_1_bsp//HAL/inc/alt_types.h:
