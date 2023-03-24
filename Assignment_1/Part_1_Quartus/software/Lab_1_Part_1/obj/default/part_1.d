d83b776beb1487536705c96e0447948b5a857eaa
Beck Busch <beckbusch5@gmail.com>
2023-03-24 16:48:49 +1300

    Lab 1 Part 1 finished

---
+++ Assignment_1/Part_1_Quartus/software/Lab_1_Part_1/obj/default/part_1.d
@@ -0,0 +1,15 @@
+obj/default/part_1.o: part_1.c ../Lab_1_Part_1_bsp/system.h \
+ ../Lab_1_Part_1_bsp/linker.h \
+ ../Lab_1_Part_1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Lab_1_Part_1_bsp//HAL/inc/io.h \
+ ../Lab_1_Part_1_bsp//HAL/inc/alt_types.h
+
+../Lab_1_Part_1_bsp/system.h:
+
+../Lab_1_Part_1_bsp/linker.h:
+
+../Lab_1_Part_1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Lab_1_Part_1_bsp//HAL/inc/io.h:
+
+../Lab_1_Part_1_bsp//HAL/inc/alt_types.h:
